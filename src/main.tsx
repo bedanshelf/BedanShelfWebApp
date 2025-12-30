0ab76734817c0ea617113de5c5fce0b4fa48e865
Pons Anthony Advento <ponsadvento08@gmail.com>
2025-12-30 16:02:55 +0800

    converted browser router to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./Components/App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./Components/Context/AuthContext.tsx";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <StrictMode>
         <App />
       </StrictMode>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
